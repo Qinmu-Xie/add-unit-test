4f78cce474601b1d823861ae35df511f2dbdb112
qmx <xieqinmu@126.com>
2019-07-10 20:37:40 +0800

    add some tests

--- src/test/java/com/github/xqm/LocaleUtilsTest.java
+++ src/test/java/com/github/xqm/LocaleUtilsTest.java
@@ -1,5 +1,6 @@
 package com.github.xqm;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Locale;
@@ -8,15 +9,127 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class LocaleUtilsTest {
+    private LocaleUtils localUtils;
+    private String testString;
+    private static String EMPTY = "";
+
+    @Before
+    public void setUp() throws Exception {
+        localUtils = new LocaleUtils();
+    }
 
     @Test
-    public void should_return_null_when_str_is_null() {
+    public void should_return_null_when_string_is_null() {
         //given
-        String testStr = null;
+        testString = null;
         //when
-        Locale res = new LocaleUtils().toLocale(testStr);
+        Locale res = localUtils.toLocale(testString);
         //then
         assertThat(res).isNull();
+    }
+
+    @Test
+    public void should_return_empty_when_string_is_empty() {
+        //given
+        testString = "";
+        //when
+        Locale res = localUtils.toLocale(testString);
+        //then
+        assertThat(res).isEqualTo(new Locale(EMPTY, EMPTY));
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_get_illegal_argument_exception_when_string_contains_special_char() {
+        //given
+        testString = "a#b";
+        //when
+        localUtils.toLocale(testString);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_get_illegal_argument_exception_when_string_contains_less_than_2_chars() {
+        //given
+        testString = "a";
+        //when
+        localUtils.toLocale(testString);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_get_illegal_argument_exception_when_crossbar_string_less_than_3_chars() {
+        //given
+        testString = "_b";
+        //when
+        localUtils.toLocale(testString);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_get_illegal_argument_exception_when_crossbar_string_not_start_with_2_uppercase_char() {
+        //given
+        testString = "_bbb";
+        //when
+        localUtils.toLocale(testString);
+    }
+
+    @Test
+    public void should_return_sub_string_when_crossbar_string_length_of_3() {
+        //given
+        testString = "_AB";
+        //when
+        Locale res = localUtils.toLocale(testString);
+        //then
+        assertThat(res).isEqualTo(new Locale(EMPTY, "AB"));
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_return_sub_string_when_crossbar_string_less_than_length_5() {
+        //given
+        testString = "_ABA";
+        //when
+        localUtils.toLocale(testString);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_return_sub_string_when_crossbar_string_contains_crossbar_at_char_3() {
+        //given
+        testString = "_ABsdf";
+        //when
+        localUtils.toLocale(testString);
+    }
+
+    @Test
+    public void should_return_local_of_correct_crossbar_string() {
+        //given
+        testString = "_AB_CD";
+        //when
+        Locale res = localUtils.toLocale(testString);
+        //then
+        assertThat(res).isEqualTo(new Locale(EMPTY, "AB", "CD"));
+
+    }
+
+    @Test
+    public void should_return_local_of_ISO639_string_length_2() {
+        //given
+        testString = "bb";
+        //when
+        Locale res = localUtils.toLocale(testString);
+        //then
+        assertThat(res).isEqualTo(new Locale(testString));
+
+    }
+
+
+    @Test
+    public void should_return_local_of_ISO3166_string_length_2() {
+        //given
+        testString = "BB";
+        //when
+        Locale res = localUtils.toLocale(testString);
+        //then
+        assertThat(res).isEqualTo(new Locale(testString));
+
     }
 
 }
